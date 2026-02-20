3351a5772db79c0d9a35b97a9fe0a6e354a7316e
sinchu1209 <sinchanasiddesh@gmail.com>
2026-02-20 20:45:48 +0530

    Add background image and responsive CSS updates

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/Button";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
 const navLinks = [
     { href: "#about" , label: "About" },
@@ -8,6 +10,7 @@ const navLinks = [
 ]
 
 export const NavBar = () => {
+    const[ isMobileMenuOpen , setIsMobileMenuOpen] = useState(false);
     return (
     <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
         <nav className="container mx-auto px-6 flex items-center justify-between">
@@ -19,7 +22,7 @@ export const NavBar = () => {
             </a>
 
             {/* Desktop Nav */}
-            <div className="flex items-center gap-1">
+            <div className="hidden md:flex items-center gap-1">
                 <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
                     {navLinks.map((link, index) => (
                         <a href={link.href} key={index} 
@@ -29,11 +32,32 @@ export const NavBar = () => {
                     ))}
                 </div>
             </div>
-
-            <div>
+            {/* CTA Button */}
+            <div className="hidden md:block">
                 <Button size="sm">Contact Me</Button>
             </div>
+
+            {/* Mobile menu Button */}
+            <button className="md:hidden p-2 text-foreground hover:text-primary cursor-pointer"
+             onClick ={() => setIsMobileMenuOpen((prev) => !prev)}>
+                    {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
+            </button>
         </nav>
+        {/* Mobile Menu */}
+        {isMobileMenuOpen &&(
+            <div className="md:hidden glass-strong animate-fade-in">
+           <div className="container mx-auto px-6 py-6 flex flex-col gap-4"> 
+             {navLinks.map((link, index) => (
+                        <a href={link.href} 
+                        key={index} 
+                        className="text-lg text-muted-foreground hover:text-primary py-2 ">
+                            {link.label}
+                            </a>
+             ))}
+             <Button size="sm">Contact Me</Button>
+           </div>
+        </div>
+    )}
     </header>
     );
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,16 @@
 export const Hero = () => {
-    return <section></section>;
-}
+    return (
+    <section className="relative min-h-screen flex items-center overflow-hidden">
+        {/* Bg */}
+        <div className="absolute inset-0">
+            <img src="/projects/hero-bg.jpg" 
+             alt="bg image"
+             className="w-full h-full object-cover opacity-40"
+            />
+            <div className="absolute inset-0 bg-gradient-to-b from-background/20 via-background/80 to-background" />
+        </div>
+
+
+    </section>
+    );
+};
